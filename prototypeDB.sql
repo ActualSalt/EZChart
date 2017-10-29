af5e4d4eaa15e60ed9831e74dcb85ec6435d829c
Min Lee <minlee0702@gmail.com>
2017-10-29 14:56:49 -0500

    Proto_DB_construction
    Barebone DB construction scripts
    
    Run the whole thing once to construct schema and tables (form and employee) then popluate table.

---
+++ prototypeDB.sql
@@ -0,0 +1,38 @@
+/*Create schema */
+CREATE SCHEMA `EZChart` ;
+
+/*Create Table
+	This table is temporary for the development
+    Decompose later*/
+CREATE TABLE `EZChart`.`Temp_form` (
+  `Form_ID` VARCHAR(10) NOT NULL,
+  `Charter_ID` VARCHAR(10) NOT NULL,
+  `Patien_ID` VARCHAR(10) NOT NULL,
+  `FName` VARCHAR(45) NULL,
+  `LName` VARCHAR(45) NULL,
+  `Height` INT NULL,
+  `Weight` INT NULL,
+  `Comment` VARCHAR(200) NULL,
+  PRIMARY KEY (`Form_ID`));
+
+/*Create employee table*/
+CREATE TABLE `EZChart`.`Employee` (
+  `EmployeeID` VARCHAR(10) NOT NULL,
+  `Title` VARCHAR(45) NOT NULL,
+  `Name` VARCHAR(45) NULL,
+  PRIMARY KEY (`EmployeeID`));
+  
+  /*Popluate employee table with three different titles */
+  INSERT INTO `EZChart`.`Employee` (`EmployeeID`, `Title`, `Name`) VALUES ('c0000', 'doc', 'John');
+INSERT INTO `EZChart`.`Employee` (`EmployeeID`, `Title`, `Name`) VALUES ('c0001', 'nur', 'Juan');
+INSERT INTO `EZChart`.`Employee` (`EmployeeID`, `Title`, `Name`) VALUES ('c0002', 'cna', 'Tue');
+
+/*View the table */
+SELECT * FROM EZChart.Temp_form;
+
+/*Test input template
+	Form_ID PK
+    Charter_ID and Patient_ID NOT NULL*/
+INSERT INTO `EZChart`.`Temp_form` 
+	(`Form_ID`, `Charter_ID`, `Patien_ID`, `FName`, `LName`, `Height`, `Weight`, `Comment`)
+	VALUES ('f0000', 'c0000', 'p0000', 'FirstTest', 'LastTest', '180', '180', 'Testing_comment_here');
