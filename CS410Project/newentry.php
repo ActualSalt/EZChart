fe7e4c0cf86d5056b0ca69302709a5a0cbcfb05b
Steven Granquist <sgranquist15@winona.edu>
2017-11-01 18:35:26 -0500

    CSS
    id to class

--- CS410Project/newentry.php
+++ CS410Project/newentry.php
@@ -20,19 +20,19 @@
 			<form>
 				<fieldset>
 					<legend>Patient info:</legend>
-					First name: <input type="text" id="PatientID" name="fname"><br>
-					Last name: <input type="text" id="PatientID" name="lname"><br>
-					Date of Birth: <input type="text" placeholder="mm/dd/yyyy" pattern="(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d" maxlength="10" id="PatientIDDOB"><br>
+					First name: <input type="text" class="patientID" name="fname"><br>
+					Last name: <input type="text" class="patientID" name="lname"><br>
+					Date of Birth: <input type="text" placeholder="mm/dd/yyyy" pattern="(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d" maxlength="10" class="patientIDDOB"><br>
 					Sex:  
 					<select name="patient">
 						<option value="male">Male</option>
 						<option value="female">Female</option>
 						<option value="other">Other</option>
 					</select>
-					Age: <input type="number" id="PatientAttributes" min="0" max="125" name="age"><br><br>
-					Height (in): <input type="number" id="PatientAttributes" name="height">
-					Weight (lbs): <input type="number" id="PatientAttributes" name="weight"><br>
-					Blood Pressure: <input type="text" id="PatientAttributes" name="bp">
+					Age: <input type="number" class="patientAttributes" min="0" max="125" name="age"><br><br>
+					Height (in): <input type="number" class="patientAttributes" name="height">
+					Weight (lbs): <input type="number" class="patientAttributes" name="weight"><br>
+					Blood Pressure: <input type="text" class="patientAttributes" name="bp">
 					<textarea rows="4" cols="50" placeholder="Enter behaviors here." name="behaviors"></textarea>
 					<textarea rows="4" cols="50" placeholder="Additional Comments." name="comments"></textarea>
 				</fieldset>
@@ -45,8 +45,8 @@
 						<option value="rn">RN</option>
 						<option value="dr">Dr.</option>
 					</select><br><br>
-					First name: <input type="text" id="ChartEntererID" name="fname"><br>
-					Last name: <input type="text" id="ChartEntererID" name="lname"><br>
+					First name: <input type="text" class="chartEntererID" name="fname"><br>
+					Last name: <input type="text" class="chartEntererID" name="lname"><br>
 				</fieldset><br>
 				<input type="submit" id="Submit" value="Submit">
 			</form>
